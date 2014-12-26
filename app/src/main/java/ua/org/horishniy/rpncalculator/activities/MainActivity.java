063a99fa5000ecdb665ca10d5f276b9097088783
AndreyRage <andreyrage@horishniy.org.ua>
2014-12-27 00:21:43 +0200

    Create app structure. Move MainActivity class to package.

---
+++ app/src/main/java/ua/org/horishniy/rpncalculator/activities/MainActivity.java
@@ -0,0 +1,16 @@
+package ua.org.horishniy.rpncalculator.activities;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+
+import ua.org.horishniy.rpncalculator.R;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rpn_calculator);
+    }
+}
