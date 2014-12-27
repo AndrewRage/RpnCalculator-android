e65e244717b256c98dc6f55d0c46f06ccfaeb8b2
AndreyRage <andreyrage@horishniy.org.ua>
2014-12-27 17:52:48 +0200

    Realize simple math function

--- app/src/main/java/ua/org/horishniy/rpncalculator/activities/MainActivity.java
+++ app/src/main/java/ua/org/horishniy/rpncalculator/activities/MainActivity.java
@@ -5,7 +5,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Switch;
+import android.widget.TextView;
 
 import ua.org.horishniy.rpncalculator.R;
 import ua.org.horishniy.rpncalculator.utils.FiloStack;
@@ -14,9 +14,20 @@ import ua.org.horishniy.rpncalculator.utils.FiloStack;
 public class MainActivity extends ActionBarActivity implements View.OnClickListener {
 
     private static final String LOG = "RageLog: ";
+
+    private static final int PLUS = 0;
+    private static final int MINUS = 1;
+    private static final int MULTIPLY = 2;
+    private static final int DIVISION = 3;
+    private static final int REVERCE = 4;
+    private static final int SIGN_CHANGE = 5;
+    private static final int ENTER = 6;
+    private static final int CLEAR = 7;
+
     private FiloStack<String> stack;
     private StringBuilder buffer;
     private boolean coma;
+    private TextView viewDisplay;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +37,8 @@ public class MainActivity extends ActionBarActivity implements View.OnClickListe
         stack = new FiloStack<>();
         buffer = new StringBuilder();
         coma = false;
+
+        viewDisplay = (TextView) findViewById(R.id.text_view_display);
 
         //Add buttons
         Button button0 = (Button) findViewById(R.id.button_0);
@@ -106,13 +119,32 @@ public class MainActivity extends ActionBarActivity implements View.OnClickListe
                 addDigit(".");
                 break;
             case R.id.button_plus:
-                stack.push(buffer.toString());
-                buffer.setLength(0);
+                math(PLUS);
+                break;
+            case R.id.button_minus:
+                math(MINUS);
+                break;
+            case R.id.button_multiply:
+                math(MULTIPLY);
+                break;
+            case R.id.button_division:
+                math(DIVISION);
+                break;
+            case R.id.button_reverse:
+                function(REVERCE);
+                break;
+            case R.id.button_sign_change:
+                function(SIGN_CHANGE);
+                break;
+            case R.id.button_enter:
+                function(ENTER);
+                break;
+            case R.id.button_clear:
+                function(CLEAR);
                 break;
         }
 
         Log.d(LOG, "Buffer: " + buffer);
-
     }
 
     private void addDigit(String d) {
@@ -132,7 +164,7 @@ public class MainActivity extends ActionBarActivity implements View.OnClickListe
             case "8":
             case "9":
                 if (!buffer.toString().equals("0")) {
-                    buffer.append("d");
+                    buffer.append(d);
                 } else {
                     buffer.setLength(0);
                     buffer.append(d);
@@ -141,9 +173,80 @@ public class MainActivity extends ActionBarActivity implements View.OnClickListe
             case ".":
                 if (!coma) {
                     coma = true;
+                    if (buffer.toString().equals("")) {
+                        buffer.append("0");
+                    }
                     buffer.append(".");
                 }
                 break;
         }
+        display(buffer.toString());
+    }
+
+    private void function(int id) {
+        switch (id) {
+            case ENTER:
+                if (!buffer.toString().equals("")) {
+                    stack.push(buffer.toString());
+                }
+                clear();
+                break;
+            case CLEAR:
+                clear();
+                stack.clear();
+                display(getString(R.string.display_default));
+                break;
+        }
+    }
+
+    private void math(int id) {
+        if (!buffer.toString().equals("")) {
+            stack.push(buffer.toString());
+            coma = false;
+        }
+        if (stack.size() >= 2) {
+            double b = Double.parseDouble(stack.peek());
+            stack.pop();
+            double a = Double.parseDouble(stack.peek());
+            stack.pop();
+            boolean error = false;
+            switch (id) {
+                case PLUS:
+                    a = a + b;
+                    break;
+                case MINUS:
+                    a = a - b;
+                    break;
+                case MULTIPLY:
+                    a = a * b;
+                    break;
+                case DIVISION:
+                    if (b != 0) {
+                        a = a / b;
+                    } else {
+                        error = true;
+                    }
+                    break;
+            }
+            if (!error) {
+                Log.d(LOG, "rez = " + a);
+                stack.push("" + a);
+                display("" + a);
+            } else {
+                display(getString(R.string.display_error));
+            }
+        } else {
+            display(getString(R.string.display_error));
+        }
+        clear();
+    }
+
+    private void clear(){
+        buffer.setLength(0);
+        coma = false;
+    }
+
+    private void display(String massage) {
+        viewDisplay.setText(massage);
     }
 }
